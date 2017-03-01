c3e85e571e52b888bc9e299523a37d669cde214a
ackamthe <ackamthe@gmail.com>
2017-03-01 23:14:05 +0530

    build is with uncleaned code for materialize clockpicker

--- src/app/main/controllers/eventCtrl.js
+++ src/app/main/controllers/eventCtrl.js
@@ -1,12 +1,20 @@
 angular.module('whpmc').controller('eventCtrl', ['$scope', 'mainService', '$rootScope', '$log', function($scope, mainService, $rootScope, $log) {
     var that = $scope;
     // var rootThat = $rootScope;
-    that.location_A = '';
+    that.createEventForm = {};
     $log.debug("Event controller loaded....");
     that.getLocationA = function() {
         $log.debug("Origin Location : ", that.origin_location);
         $log.debug("Destination Location : ", that.destination_location);
-        $log.debug("Destination Location : ", that.event_date);
+
+
+        that.createEventForm.origin_lat = that.origin_location[3],
+            that.createEventForm.origin_lng = that.origin_location[4],
+            that.createEventForm.destination_lat = that.destination_location[3],
+            that.createEventForm.destination_lng = that.destination_location[4]
+
+        $log.debug("Whole Info : ", that.createEventForm);
+
     }
 
 
